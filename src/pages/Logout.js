a01ed391d8c74c7f0f0c0926b51a7cbf83584877
DavidGjorgjievski <david.gjorgjievski@students.finki.ukim.mk>
2024-11-10 14:41:53 +0100

    modify the logout.js

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,32 +1,18 @@
 import React, { useEffect } from 'react';
-import axios from 'axios'; 
 import '../styles/Logout.css'; 
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const Logout = () => {
 
     useEffect(() => {
-        const logoutUser = async () => {
-            try {
-                await axios.post(process.env.REACT_APP_API_URL + '/api/logout', null, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('jwtToken')}` 
-                    }
-                });
+    const logoutUser = () => {
+        localStorage.removeItem('jwtToken');
+        localStorage.removeItem('userInfo');
+        console.log('Successfully logged out');
+    };
 
-                // Clear the token and userInfo
-                localStorage.removeItem('jwtToken');
-                localStorage.removeItem('userInfo'); 
-
-                console.log('Successfully logged out');
-            } catch (error) {
-                console.error('Error logging out:', error);
-            }
-        };
-
-        logoutUser();
-    }, []);
-
+    logoutUser();
+}, []);
     return (
         <div className="logout-container">
             <HelmetProvider>
