c034df4c7c65df52abdf4efc8fc8412cfcf61ec4
DavidGjorgjievski <david.gjorgjievski@students.finki.ukim.mk>
2024-12-23 11:37:52 +0100

    max file size on topic 20MB

--- src/util/fileUpload.js
+++ src/util/fileUpload.js
@@ -1,4 +1,4 @@
-export const MAX_FILE_SIZE_MB = 10;
+export const MAX_FILE_SIZE_MB = 20;
 export const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 export const handleDragOver = (event, dropArea) => {
